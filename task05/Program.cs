f7eaac80c30457923138e0bb3fc916826d72af1e
Tishchenko Aleksey <aleksey-rock@hotmail.com>
2023-10-05 16:43:57 +0700

    Семинар закончен

---
+++ task05/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Clear();
+
+
+System.Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+for(int i = -userNumber; i <= userNumber; i++)
+{
+    System.Console.Write(i + " ");
+}
